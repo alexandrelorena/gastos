fd4bc04dc90a109794d7d4a4cfaf3bd22fe483fd
alexandrelorena <alexandre.lorena@gmail.com>
2024-06-15 20:43:20 -0300

    atualizações

--- gastos/settings.py
+++ gastos/settings.py
@@ -11,21 +11,26 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# Inicialize o django-environ
+env = environ.Env(
+    # Defina variáveis padrão se necessário
+    DEBUG=(bool, False)
+)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!hf)5cecy^%dv!hkk7g3s*atb_x$rjd_%&um=%07cql3k!1_!g"
+# Leia o arquivo .env
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# ALLOWED_HOSTS = ['*']
 
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -77,17 +82,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "gastos.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gastos',
-        'USER': 'postgres',
-        'PASSWORD': 'alexloo2357',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
